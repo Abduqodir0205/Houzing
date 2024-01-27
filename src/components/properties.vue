4fe7f1c4c36f691c5fbe9ac1818b2ff62c743cbc
Abduqodir0205 <abduqodirdeveloper@gmail.com>
2024-01-27 20:45:15 +0500

    ...

--- src/components/properties.vue
+++ src/components/properties.vue
@@ -125,50 +125,160 @@
               <label for="">Map location</label>
             </span>
             <div class="main-properties__content-location-map">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d16019.61029816283!2d69.22230813249418!3d41.23730550362097!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sru!2s!4v1706192906709!5m2!1sru!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d16019.61029816283!2d69.22230813249418!3d41.23730550362097!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sru!2s!4v1706192906709!5m2!1sru!2s"
+                width="600"
+                height="450"
+                style="border: 0"
+                allowfullscreen=""
+                loading="lazy"
+                referrerpolicy="no-referrer-when-downgrade"
+              ></iframe>
             </div>
             <div>
-                <span>
-                    <input type="text">
-                    <label for="">Latidude</label>
-                </span>
-                <span>
-                    <input type="text">
-                    <label for="">Logtitude</label>
-                </span>
+              <span>
+                <input type="text" />
+                <label for="">Latidude</label>
+              </span>
+              <span>
+                <input type="text" />
+                <label for="">Logtitude</label>
+              </span>
             </div>
           </form>
         </div>
         <div class="main-properties__content-media main-properties">
-            <h2>Media</h2>
-            <div class="main-properties__content-media-images">
-                <p>Featured image</p>
-                <div>
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                </div>
-                <button>Upload</button>
+          <h2>Media</h2>
+          <div class="main-properties__content-media-images">
+            <p>Featured image</p>
+            <div>
+              <span>
+                <div v-for="element in default"></div>
+              </span>
+              <div v-for="(image, key) in images">
+                <img v-bind:ref="'image' + parseInt(key)" alt="Error" />
+              </div>
             </div>
-            <div class="main-properties__content-media-gallary">
-                <p>Gallery</p>
-                <button>Upload</button>
+            <span>
+              <label for="upload-photo">Upload</label>
+              <input
+                type="file"
+                @input="onFileChange"
+                name="photo"
+                multiple
+                id="upload-photo"
+              />
+            </span>
+          </div>
+          <div class="main-properties__content-media-gallary">
+            <p>Gallery</p>
+            <div class="main-properties__content-media-gallary-img">
+              <div v-for="(image, key) in gallary">
+                <img v-bind:ref="'image' + parseInt(key)" alt="Error" />
+              </div>
             </div>
-            <div class="main-properties__content-media-attachment">
-                <p>Attachment</p>
-                <span>
-                    <img src="../assets/images/svg/file 1 (1).svg" alt="">
-                    <p>test_property.pdf</p>
-                </span>
-                <button>Upload</button>
-            </div>
+            <span>
+              <label for="upload-photo">Upload</label>
+              <input
+                type="file"
+                @input="onFileChangeGallare"
+                name="photo"
+                multiple
+                id="upload-photo"
+              />
+            </span>
+          </div>
+          <div class="main-properties__content-media-attachment">
+            <p>Attachment</p>
+            <span>
+              <img src="../assets/images/svg/file 1 (1).svg" alt="" />
+              <p>test_property.pdf</p>
+            </span>
+            <span>
+              <label for="upload-photo">Upload</label>
+              <input
+                type="file"
+                @input="onFileChange"
+                name="photo"
+                multiple
+                id="upload-photo"
+              />
+            </span>
+          </div>
+
+          <div class="main-properties__content-media-videoLink">
+            <form action=""></form>
+          </div>
+
         </div>
       </div>
     </div>
   </main>
 </template>
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      urls: [],
+      images: [],
+      gallary: [],
+      show: false,
+      default: 4,
+    };
+  },
+  methods: {
+    onFileChangeGallare(e) {
+      var selectedFiles = e.target.files;
+      for (let i = 0; i < selectedFiles.length; i++) {
+        this.gallary.push(selectedFiles[i]);
+      }
+      for (let i = 0; i < this.gallary.length; i++) {
+        let reader = new FileReader(); //instantiate a new file reader
+        reader.addEventListener(
+          "load",
+          function () {
+            this.$refs["image" + parseInt(i)][0].src = reader.result;
+          }.bind(this),
+          false
+        ); //add event listener
+
+        reader.readAsDataURL(this.gallary[i]);
+      }
+    },
+    onFileChange(e) {
+      this.show = true;
+      var selectedFiles = e.target.files;
+      for (let i = 0; i < selectedFiles.length; i++) {
+        this.images.push(selectedFiles[i]);
+        if (this.default > 0) {
+          this.default--;
+        } else {
+          continue;
+        }
+      }
+      for (let i = 0; i < this.images.length; i++) {
+        let reader = new FileReader(); //instantiate a new file reader
+        reader.addEventListener(
+          "load",
+          function () {
+            this.$refs["image" + parseInt(i)][0].src = reader.result;
+          }.bind(this),
+          false
+        ); //add event listener
+
+        reader.readAsDataURL(this.images[i]);
+      }
+
+      // console.log(this.images);
+    },
+    removeImage(i) {
+      // alert(i);
+
+      var arrayImages = this.images;
+      var index = arrayImages.indexOf(arrayImages[i]);
+      arrayImages.splice(index, i);
+    },
+  },
+};
 </script>
-<style lang=""></style>
+<style></style>
